ce90a0db6ed1d607b0ff9d7dbc8ed2a1de2f04d1
handigarde <rhandy87@gmail.com>
2014-07-13 10:21:42 -0400

    Load maximum m addresses and distances per call to google maps API

--- application.py
+++ application.py
@@ -27,6 +27,27 @@ def get_distance_address(start_point, end_point):
     distance = result['rows'][0]['elements'][0]['distance']['text']
     address = result['destination_addresses'][0]
     return distance, address
+    
+def get_distances_and_addresses(start_point, destinations):
+    distance_url = "http://maps.googleapis.com/maps/api/distancematrix/json?origins="+start_point+"&destinations="
+    i=0
+    multiplier=0
+    distances = []
+    addresses = []
+    while i < len(destinations):
+        temp_distance_url = distance_url+destinations[i]
+        i+=1
+        for n in range(24):
+            temp_distance_url = temp_distance_url +'|'+ destinations[i]
+            i += 1
+        temp_distance_url = temp_distance_url + "&mode=walking&language=en-EN&sensor=false&units=imperial"
+        result = json.load(urllib.urlopen(temp_distance_url))
+        for address in result['destination_addresses']:
+            addresses.append(address)
+        for element in result['rows'][0]['elements']:
+            distances.append(element['distance']['text'])
+    return distances, addresses
+    
 
 def load_cart_info(url):
     #Create request object to request the aforementioned list
@@ -117,19 +138,22 @@ def send_nearby_carts(lat_long, category='Anything'):
     lat_long = lat_long.split(',')
     latitude = float(lat_long[0])
     longitude = float(lat_long[1])
+    unsorted_result=[]
+    unsorted_result_lat_long=[]
     result_feet = []
     result_miles = []
     for index in find_nearby_carts(longitude, latitude, IDX):
         if index in TAGS_BY_TRUCK[category]:
-            start_point = lat_long[0] + ',' + lat_long[1]
-            end_point = str(CARTS[index]['latitude'])+','+str(CARTS[index]['longitude'])
-            distance, address = get_distance_address(start_point, end_point)
-            CARTS[index]['distance'] = distance
-            CARTS[index]['address'] = address
-            if 'ft' in CARTS[index]['distance']:
-                result_feet.append(CARTS[index])
-            else:
-                result_miles.append(CARTS[index])
+            unsorted_result.append(CARTS[index])
+            unsorted_result_lat_long.append(CARTS[index]['latitude']+','+CARTS[index]['longitude'])
+    distances, addresses = get_distances_and_addresses(lat_long[0]+','+lat_long[1], unsorted_result_lat_long)
+    for i in range(len(unsorted_result)):
+        unsorted_result[i]['distance'] = distances[i]
+        unsorted_result[i]['address'] = addresses[i]
+        if 'ft' in unsorted_result[i]['distance']:
+            result_feet.append(unsorted_result[i])
+        else:
+            result_miles.append(unsorted_result[i])
     result_feet.sort(key=operator.itemgetter('distance'))
     result_miles.sort(key=operator.itemgetter('distance'))
     result = result_feet + result_miles
